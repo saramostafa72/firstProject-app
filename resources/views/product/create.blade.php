13e152cc5c8b516e3403f857ebd39be75c85a238
saramostafa72 <99365927+saramostafa72@users.noreply.github.com>
2023-08-31 18:52:12 +0300

    Add files via upload

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -10,12 +10,48 @@
     <form action="{{route('product.store')}}" method="post" enctype="multipart/form-data">
       @csrf
      <input type="text" name="name" placeholder="product_name" >
+     @if ($errors->has('name'))
+     <span style="color: red">
+         {{ $errors->first('name') }}
+     </span>
+     @endif
+     <br>
+     <br>
      <input type="text" name="price" placeholder="product_price">
+     @if ($errors->has('price'))
+     <span style="color: red">
+         {{ $errors->first('price') }}
+     </span>
+     @endif
+     <br>
+     <br>
      <input type="text" name="availability"placeholder="product_availability">
+     @if ($errors->has('availability'))
+     <span style="color: red">
+         {{ $errors->first('availability') }}
+     </span>
+     @endif
+     <br>
+     <br>
      <input type="text" name="category_id"placeholder="category_id">
+     @if ($errors->has('category_id'))
+     <span style="color: red">
+         {{ $errors->first('category_id') }}
+     </span>
+     @endif
+     <br>
+     <br>
      <input type="file" name="image">
-
+     <br>
+     @if ($errors->has('image'))
+     <span style="color: red">
+         {{ $errors->first('image') }}
+     </span>
+     @endif
+     <br>
+     <br>
      <input type="submit" >
     </form>
+
 </body>
 </html>
